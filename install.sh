5ed69ab601748cbf8929187aaba7b05bb32179c1
alireza <alireza.f.7596@gmail.com>
2024-08-03 20:21:20 +0330

    uppdate Checklist.txt & install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-chown alireza:alireza -R ./volumes
+chown alireza:alireza -R ./volumes/nginx
+chown nobody:alireza -R ./volumes/litespeed/sites
+
+find ./volumes/litespeed/sites -type f -exec chmod 0660 {} \;
+find ./volumes/litespeed/sites -type d -exec chmod 0770 {} \;
